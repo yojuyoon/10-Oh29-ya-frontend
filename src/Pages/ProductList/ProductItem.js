ea586af119c717a0650aa3b418ac566912ac78de
hyeonjiya <imhyeonji0118@gmail.com>
2020-07-22 15:30:53 +0900

    Add: 레이아웃 수정중 백업

--- src/Pages/ProductList/ProductItem.js
+++ src/Pages/ProductList/ProductItem.js
@@ -1,14 +1,44 @@
 import React from "react";
-import "./ProductList.scss";
+import "./ProductItem.scss";
 
 class ProductItem extends React.Component {
   render() {
+    const { data } = this.props;
+    console.log(data);
+
     return (
       <li className="itemContainer">
         <div className="imgBox">
-          {/* <img src="//img.29cm.co.kr/next-product/2020/07/20/c97515156b3b4cd191004c9069eed367_20200720160946.jpg?width=500" /> */}
+          <img src="//img.29cm.co.kr/next-product/2020/07/20/c97515156b3b4cd191004c9069eed367_20200720160946.jpg?width=500" />
         </div>
-        <div className="content"></div>
+        <div className="info">
+          <div className="brand">{data[0].brand}</div>
+          <div className="name">{data[0].name}</div>
+          <div className="price">
+            <div className="sellPrice">
+              <span>{data[0].sellPrice}</span>
+            </div>
+            <div className="discountedPrice">
+              <span>{data[0].discountRate}</span>
+              <span>{data[0].discountedPrice}</span>
+              <span>원</span>
+            </div>
+          </div>
+        </div>
+        <div className="heartArea">
+          <div className="heart">
+            <div className="image">
+              <img src="Images/test_heart2.png" />
+            </div>
+            <div className="count">1,189</div>
+          </div>
+          <div className="review">
+            <div className="image">
+              <img src="Images/test_heart2.png" />
+            </div>
+            <div className="count">265</div>
+          </div>
+        </div>
       </li>
     );
   }
--- src/Pages/ProductList/ProductList.js
+++ src/Pages/ProductList/ProductList.js
@@ -1,22 +1,101 @@
 import React from "react";
 import "./ProductList.scss";
 import "../../Styles/reset.scss"; //지울거
+import { Route, Link } from "react-router-dom";
 
 import ProductItem from "./ProductItem";
 
 class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemData: [
+        {
+          img: "/images/hyeonji/cat/cat1.jpg",
+          brand: "폴로 랄프로렌",
+          name: "폴로 랄프로렌 남성 라운드 반팔티셔츠 컬렉션 (17color)",
+          sellPrice: "109,000",
+          discountRate: "82%",
+          discountedPrice: "19,900",
+        },
+        {
+          img: "/images/hyeonji/cat/cat1.jpg",
+          brand: "폴로 랄프로렌",
+          name: "폴로 랄프로렌 남성 라운드 반팔티셔츠 컬렉션 (17color)",
+          sellPrice: "109,000",
+          discountRate: "82%",
+          discountedPrice: "19,900",
+        },
+        {
+          img: "/images/hyeonji/cat/cat1.jpg",
+          brand: "폴로 랄프로렌",
+          name: "폴로 랄프로렌 남성 라운드 반팔티셔츠 컬렉션 (17color)",
+          sellPrice: "109,000",
+          discountRate: "82%",
+          discountedPrice: "19,900",
+        },
+        {
+          img: "/images/hyeonji/cat/cat1.jpg",
+          brand: "폴로 랄프로렌",
+          name: "폴로 랄프로렌 남성 라운드 반팔티셔츠 컬렉션 (17color)",
+          sellPrice: "109,000",
+          discountRate: "82%",
+          discountedPrice: "19,900",
+        },
+        {
+          img: "/images/hyeonji/cat/cat1.jpg",
+          brand: "폴로 랄프로렌",
+          name: "폴로 랄프로렌 남성 라운드 반팔티셔츠 컬렉션 (17color)",
+          sellPrice: "109,000",
+          discountRate: "82%",
+          discountedPrice: "19,900",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="ProductList">
         <div className="categoryWrap">
-          <div className="leftNav">
+          <div className="left">
             <div className="fakeNav">이야이</div>
           </div>
-          <div className="rightItems">
-            <div className="categoryList"></div>
+          <div className="right">
+            <div className="categoryTab">
+              <ul className="categoryList">
+                <li>
+                  <Link to="/">전체</Link>
+                </li>
+                <li>
+                  <Link to="/">반팔</Link>
+                </li>
+                <li>
+                  <Link to="/">긴팔</Link>
+                </li>
+                <li>
+                  <Link to="/">민소매</Link>
+                </li>
+                <li>
+                  <Link to="/">피케</Link>
+                </li>
+                <li>
+                  <Link to="/">스웨트 셔츠</Link>
+                </li>
+                <li>
+                  <Link to="/">후디</Link>
+                </li>
+                <li>
+                  <Link to="/">집업</Link>
+                </li>
+              </ul>
+            </div>
             <div className="productContent">
-              <ul>
-                <ProductItem />
+              <ul className="productSection">
+                {this.state.itemData.map((item, i) => {
+                  return <ProductItem data={this.state.itemData} key={i} />;
+                })}
+                {/* <ProductItem data={this.state.itemData} /> */}
               </ul>
             </div>
           </div>
