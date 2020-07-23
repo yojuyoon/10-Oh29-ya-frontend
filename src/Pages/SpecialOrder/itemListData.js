5e1ee051f7c79c119f5d601eefd543267342d40b
KIM <rudqo14@gmail.com>
2020-07-23 19:45:53 +0900

    Add : Special-Order 페이지 레이아웃 구현 완료

---
+++ src/Pages/SpecialOrder/itemListData.js
@@ -0,0 +1,47 @@
+export const itemList = {
+  item: [
+    {
+      index: 1,
+      img: "Images/orderImg1.jpg",
+      title: "뉴트로 스타일로 돌아온 오비라거 랄라베어",
+      subtitle: "29cm 단독, 오비라거 랄라베어 MD 한정판 4종",
+      date: "20.07.21 ~ 08.10",
+    },
+    {
+      index: 2,
+      img: "Images/orderImg2.jpg",
+      title: "쿼드 카메라를 장착한 20만 원대 스마트폰",
+      subtitle: "보급형 스마트폰의 혁신",
+      date: "20.07.20 ~ 07.31",
+    },
+    {
+      index: 3,
+      img: "Images/orderImg3.jpg",
+      title: "손상모, 염색모, 비듬을 완화해 줄 앰플 샴푸",
+      subtitle: "두피 고민, 피부와 함께 씻어 흘려보내세요.",
+      date: "20.07.16 ~ 07.29",
+    },
+    {
+      index: 4,
+      img: "Images/orderImg4.jpg",
+      title: "피부 타입과 고민에 따른 나만의 맞춤 크림",
+      subtitle: "라인과 체형에 따라 준비된 총 20가지 스킨 케어 솔루션",
+      date: "20.07.10 ~ 07.23",
+    },
+    {
+      index: 5,
+      img: "Images/orderImg5.jpg",
+      title: "나의 첫 스마트워치",
+      subtitle:
+        "29cm 단독, 7가지 스포츠 모드와 다양한 기능을 갖춘 하빗 스마트 워치",
+      date: "20.07.09 ~ 07.31",
+    },
+    {
+      index: 6,
+      img: "Images/orderImg6.jpg",
+      title: "올여름 내리 함께할 플립플랍, 코모레비",
+      subtitle: "29CM 단독 6가지 컬러 출시 ~39%",
+      date: "20.07.02 ~ 08.03",
+    },
+  ],
+};
