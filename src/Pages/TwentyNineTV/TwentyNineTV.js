8a85329f7d851e268c066411cc5e50161496cc8b
yojuyoon <60738400+yojuyoon@users.noreply.github.com>
2020-07-23 20:28:44 +0900

    conflict 해결 후 재업로드

--- src/Pages/TwentyNineTV/TwentyNineTV.js
+++ src/Pages/TwentyNineTV/TwentyNineTV.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
-<<<<<<< HEAD
 import TwentyNineTVFeedComponent from "./TwentyNineTVFeedComponent";
 import FeedModal from "./FeedModal";
 import "./TwentyNineTV.scss";
@@ -94,12 +93,6 @@ class TwentyNineTV extends React.Component {
         )}
       </div>
     );
-=======
-
-class TwentyNineTV extends React.Component {
-  render() {
-    return <div></div>;
->>>>>>> master
   }
 }
 export default withRouter(TwentyNineTV);
