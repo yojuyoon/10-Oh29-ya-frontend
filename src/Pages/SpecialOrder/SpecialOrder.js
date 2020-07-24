babaafc392d235e171a8ebd62afadf8ab24c8f42
KIM <rudqo14@gmail.com>
2020-07-25 00:16:37 +0900

    Add : SpecialOrder 페이지 레이아웃 구현

--- src/Pages/SpecialOrder/SpecialOrder.js
+++ src/Pages/SpecialOrder/SpecialOrder.js
@@ -4,12 +4,37 @@ import { itemList } from "./itemListData";
 import "./SpecialOrder.scss";
 
 class Login extends React.Component {
+  state = {
+    dDay: 0,
+  };
+
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  tick() {
+    this.setState({
+      dDay: new Date("2020/07/31 00:00:00").getTime() - new Date().getTime(),
+    });
+  }
+
+  digitCheck = (time) => {
+    return time < 10 ? `0${time}` : time;
+  };
+
   render() {
+    const { dDay } = this.state;
+    const { digitCheck } = this;
+
     return (
       <div className="SpecialOrder">
         <ul className="itemContainer">
           {itemList.item.map(
-            ({ index, img, title, subtitle, date, subcontent }) => (
+            ({ index, img, title, subtitle, date, subcontent, progress }) => (
               <li key={index} className="itemList">
                 <div className="imgContainer">
                   <img className="orderImg" alt="img" src={img} />
@@ -20,11 +45,26 @@ class Login extends React.Component {
                   <span className="subcontent">{subcontent}</span>
                 </h4>
                 <div className="dDayBubble">
-                  {index === 4 ? (
-                    <p className="bubble">D-day 00:39:51</p>
+                  {index === 1 && dDay >= 0 ? (
+                    <div className="bubble">
+                      D-day{" "}
+                      {digitCheck(
+                        Math.floor(
+                          (dDay % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+                        )
+                      )}
+                      {":"}
+                      {digitCheck(
+                        Math.floor((dDay % (1000 * 60 * 60)) / (1000 * 60))
+                      )}
+                      {":"}
+                      {digitCheck(Math.floor((dDay % (1000 * 60)) / 1000))}
+                    </div>
                   ) : null}
                 </div>
-                <div className="dDayBorder" />
+                <div className="dDayBorder">
+                  <div className={progress} />
+                </div>
                 <p className="itemDate">{date}</p>
               </li>
             )
--- src/Pages/SpecialOrder/itemListData.js
+++ src/Pages/SpecialOrder/itemListData.js
@@ -1,11 +1,21 @@
 export const itemList = {
   item: [
     {
-      index: 1,
+      index: 0,
       img: "Images/orderImg1.jpg",
       title: "뉴트로 스타일로 돌아온 오비라거 랄라베어",
       subtitle: "29cm 단독, 오비라거 랄라베어 MD 한정판 4종",
       date: "20.07.21 ~ 08.10",
+      progress: "progress obLager",
+    },
+    {
+      index: 1,
+      img: "Images/Oh29ya.jpg",
+      title: "> Wecode 부트캠프",
+      subtitle: "17기 얼리버드 할인 진행중 ",
+      subcontent: "(얼리버드 잔여: 10)",
+      date: "20.07.11 ~ 07.24",
+      progress: "progress wecode",
     },
     {
       index: 2,
@@ -13,6 +23,7 @@ export const itemList = {
       title: "쿼드 카메라를 장착한 20만 원대 스마트폰",
       subtitle: "보급형 스마트폰의 혁신",
       date: "20.07.20 ~ 07.31",
+      progress: "progress samsung",
     },
     {
       index: 3,
@@ -20,29 +31,25 @@ export const itemList = {
       title: "손상모, 염색모, 비듬을 완화해 줄 앰플 샴푸",
       subtitle: "두피 고민, 피부와 함께 씻어 흘려보내세요.",
       date: "20.07.16 ~ 07.29",
+      progress: "progress pibuPibu",
     },
+
     {
       index: 4,
-      img: "Images/Oh29ya.jpg",
-      title: "> Wecode 부트캠프",
-      subtitle: "17기 얼리버드 할인 진행중 ",
-      subcontent: "(얼리버드 잔여: 10)",
-      date: "20.07.10 ~ 07.23",
-    },
-    {
-      index: 5,
       img: "Images/orderImg5.jpg",
       title: "나의 첫 스마트워치",
       subtitle:
         "29cm 단독, 7가지 스포츠 모드와 다양한 기능을 갖춘 하빗 스마트 워치",
       date: "20.07.09 ~ 07.31",
+      progress: "progress havit",
     },
     {
-      index: 6,
+      index: 5,
       img: "Images/orderImg6.jpg",
       title: "올여름 내리 함께할 플립플랍, 코모레비",
       subtitle: "29CM 단독 6가지 컬러 출시 ~39%",
       date: "20.07.02 ~ 08.03",
+      progress: "progress komorebi",
     },
   ],
 };
